8a2e72885889cde99cdf1bcd99f4ca29cbef752c
María José Issa <95626214+Majoissa@users.noreply.github.com>
2024-03-12 12:52:05 +0100

    solving issues

--- App.js
+++ App.js
@@ -9,7 +9,10 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
