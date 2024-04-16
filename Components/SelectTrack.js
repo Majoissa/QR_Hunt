34b0bc24926de786b945779c6d181a01726be759
María José Issa <95626214+Majoissa@users.noreply.github.com>
2024-04-16 13:17:35 +0200

    T022-Add clue scrollable component, T023-Button component (1) y T024-Cancel button component (1)

--- Components/SelectTrack.js
+++ Components/SelectTrack.js
@@ -1,41 +1,78 @@
-import React from 'react';
-import { View, Image, TouchableOpacity, StyleSheet, Text } from 'react-native';
-import CodeOverlay from './CodeOverlay';
+import React from "react";
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  ScrollView,
+} from "react-native";
+import CodeOverlay from "./CodeOverlay";
 
 const CustomComponent = () => {
   return (
     <View style={styles.container}>
-      {/*<CodeOverlay />*/}
-      <Text style={styles.titleText}>Añadir pista de</Text>
-      <View style={styles.gridContainer}>
-        <View style={styles.rowContainer}>
-          <TouchableOpacity style={styles.button}>
-            <Image source={require('../images/Image_Button.png')} style={styles.backgroundImage} />
-            <Image source={require('../images/Image_Icon.png')} style={styles.centeredForegroundImage} resizeMode="contain" />
-            <Text style={styles.labelText}>Imagen</Text>
-          </TouchableOpacity>
-          <View style={styles.separator} />
-          <TouchableOpacity style={styles.button}>
-            <Image source={require('../images/Text_Button.png')} style={styles.backgroundImage} />
-            <Image source={require('../images/icon_text.png')} style={styles.centeredForegroundImage} resizeMode="contain" />
-            <Text style={styles.labelText}>Texto</Text>
-          </TouchableOpacity>
+      <ScrollView>
+        {/*<CodeOverlay />*/}
+        <Text style={styles.titleText}>Añadir pista de</Text>
+        <View style={styles.gridContainer}>
+          <View style={styles.rowContainer}>
+            <TouchableOpacity style={styles.button}>
+              <Image
+                source={require("../images/Image_Button.png")}
+                style={styles.backgroundImage}
+              />
+              <Image
+                source={require("../images/Image_Icon.png")}
+                style={styles.centeredForegroundImage}
+                resizeMode="contain"
+              />
+              <Text style={styles.labelText}>Imagen</Text>
+            </TouchableOpacity>
+            <View style={styles.separator} />
+            <TouchableOpacity style={styles.button}>
+              <Image
+                source={require("../images/Text_Button.png")}
+                style={styles.backgroundImage}
+              />
+              <Image
+                source={require("../images/icon_text.png")}
+                style={styles.centeredForegroundImage}
+                resizeMode="contain"
+              />
+              <Text style={styles.labelText}>Texto</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.verticalSeparator} />
+          <View style={styles.rowContainer}>
+            <TouchableOpacity style={styles.button}>
+              <Image
+                source={require("../images/Mic_Button.png")}
+                style={styles.backgroundImage}
+              />
+              <Image
+                source={require("../images/icon_audio.png")}
+                style={styles.centeredForegroundImage}
+                resizeMode="contain"
+              />
+              <Text style={styles.labelText}>Audio</Text>
+            </TouchableOpacity>
+            <View style={styles.separator} />
+            <TouchableOpacity style={styles.button}>
+              <Image
+                source={require("../images/Geo_Button.png")}
+                style={styles.backgroundImage}
+              />
+              <Image
+                source={require("../images/icon_geo.png")}
+                style={styles.centeredForegroundImage}
+                resizeMode="contain"
+              />
+              <Text style={styles.labelText}>Geolocalizacion</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={styles.verticalSeparator} />
-        <View style={styles.rowContainer}>
-          <TouchableOpacity style={styles.button}>
-            <Image source={require('../images/Mic_Button.png')} style={styles.backgroundImage} />
-            <Image source={require('../images/icon_audio.png')} style={styles.centeredForegroundImage} resizeMode="contain" />
-            <Text style={styles.labelText}>Audio</Text>
-          </TouchableOpacity>
-          <View style={styles.separator} />
-          <TouchableOpacity style={styles.button}>
-            <Image source={require('../images/Geo_Button.png')} style={styles.backgroundImage} />
-            <Image source={require('../images/icon_geo.png')} style={styles.centeredForegroundImage} resizeMode="contain" />
-            <Text style={styles.labelText}>Geolocalizacion</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      </ScrollView>
     </View>
   );
 };
@@ -43,17 +80,17 @@ const CustomComponent = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    paddingBottom: 20,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingVertical: 40,
   },
   gridContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   rowContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around",
     marginBottom: 10,
   },
   verticalSeparator: {
@@ -65,33 +102,33 @@ const styles = StyleSheet.create({
   button: {
     width: 150,
     height: 120,
-    alignItems: 'center',
+    alignItems: "center",
     borderRadius: 10,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   backgroundImage: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
   centeredForegroundImage: {
     marginTop: 20,
-    width: '40%',
-    height: '40%',
+    width: "40%",
+    height: "40%",
   },
   labelText: {
     marginTop: 10,
     fontSize: 16,
-    fontWeight: 'bold',
-    color: 'white',
-    textAlign: 'center',
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
   },
   titleText: {
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
     marginBottom: 10,
   },
 });
