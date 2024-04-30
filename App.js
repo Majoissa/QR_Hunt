796181e89b94df20141d23ee9115b32f71cc6f05
María José Issa <95626214+Majoissa@users.noreply.github.com>
2024-04-30 11:41:23 +0200

    initialRouteName change

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import NewGameScreen from "./Screens/NewGameScreen";
-import {StyleSheet} from "react-native";
-import {createStackNavigator} from "@react-navigation/stack";
-import {NavigationContainer, useNavigation} from "@react-navigation/native";
+import { StyleSheet } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import HomeScreen from "./Screens/HomeScreen";
 import SelectorGameScreen from "./Screens/SelectorGameScreen";
 import CodeOverlay from "./Components/CodeOverlay";
-
 
 const Stack = createStackNavigator();
 
@@ -14,23 +13,23 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="NewGame"
+        initialRouteName="Home"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="MyGames" component={SelectorGameScreen} />
         <Stack.Screen name="NewGame" component={NewGameScreen} />
-        <Stack.Screen name="Overlay" component={CodeOverlay}/>
+        <Stack.Screen name="Overlay" component={CodeOverlay} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
