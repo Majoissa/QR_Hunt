3745ae0d181442b212e8d434372c8ea0690080fd
Joan Lopez <johanuab@gmail.com>
2024-05-07 11:42:41 +0200

    Create AddGeolocalization.js

---
+++ Screens/AddGeolocalization.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Alert, Image } from 'react-native';
+
+const AddGeolocalization = () => {
+    return (
+        <View>
+            <Text>Geo</Text>
+        </View>
+    );
+};
+
+export default AddGeolocalization;
