f25e2c980562b89ccbbd694938c4d16c9f0f92a0
María José Issa <95626214+Majoissa@users.noreply.github.com>
2024-05-21 13:23:06 +0200

    overlay colors, codeOverlay added to geolocalization

--- Screens/AddImageClue.js
+++ Screens/AddImageClue.js
@@ -1,19 +1,25 @@
 // Importa los componentes necesarios de React y React Native
-import React, {useState} from 'react';
-import { View, Text, StyleSheet, ImageBackground, ScrollView, Dimensions } from 'react-native';
-import Navbar from '../Components/Navbar';
-import SaveClue from '../Components/SaveClueButton';
-import SaveBtn from '../images/Btn_CrearPartida.png';
-import CoverImgSelector from '../Components/CoverImgSelector';
-import TitleInput from '../Components/TitleInput';
-import decorateMapComponent from 'react-native-maps/lib/decorateMapComponent';
-import DescriptionInput from '../Components/DescriptionInput';
-import CodeOverlay from '../Components/CodeOverlay';
-import ActiveCodeOverlay from '../Components/ActiveCodeOverlay';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+import Navbar from "../Components/Navbar";
+import SaveClue from "../Components/SaveClueButton";
+import SaveBtn from "../images/Btn_CrearPartida.png";
+import CoverImgSelector from "../Components/CoverImgSelector";
+import TitleInput from "../Components/TitleInput";
+import decorateMapComponent from "react-native-maps/lib/decorateMapComponent";
+import DescriptionInput from "../Components/DescriptionInput";
+import CodeOverlay from "../Components/CodeOverlay";
+import ActiveCodeOverlay from "../Components/ActiveCodeOverlay";
 
 // Define el componente AddImageClue
 const AddImageClue = () => {
-  
   const [isOverlayVisible, setIsOverlayVisible] = useState(false);
   const [code, setCode] = useState("ABC123...");
 
@@ -31,60 +37,70 @@ const AddImageClue = () => {
 
   return (
     <ImageBackground
-    source={require('../images/Background_Hint_Image.png')}
-    style={styles.backgroundImage}
+      source={require("../images/Background_Hint_Image.png")}
+      style={styles.backgroundImage}
     >
-    <Navbar title="Crear Pista Imagen"/>
-    <View style={{ padding: 20, paddingBottom: 30, height: '80%' }}>
-      <ScrollView>
-      <CoverImgSelector/>
-        <TitleInput/>
-        <DescriptionInput/>
-        <View style={{ paddingBottom: 20 }}>
-            <CodeOverlay onEditPress={handlePress} code={code} onCodeChange={handleCodeChange} />
+      <Navbar title="Crear Pista Imagen" />
+      <View style={{ padding: 20, paddingBottom: 30, height: "80%" }}>
+        <ScrollView>
+          <CoverImgSelector />
+          <TitleInput />
+          <DescriptionInput />
+          <View style={{ paddingBottom: 20 }}>
+            <CodeOverlay
+              onEditPress={handlePress}
+              code={code}
+              onCodeChange={handleCodeChange}
+              colors={{
+                panelBackground: "#003B2C",
+                qrButton: "#3a7356",
+              }}
+            />
           </View>
-         
-      </ScrollView>
-      </View>  
+        </ScrollView>
+      </View>
       {isOverlayVisible && (
         <View style={styles.overlay}>
-          <ActiveCodeOverlay code={code} onCodeChange={handleCodeChange} onAccept={handleAccept} />
+          <ActiveCodeOverlay
+            code={code}
+            onCodeChange={handleCodeChange}
+            onAccept={handleAccept}
+          />
         </View>
       )}
- 
-    <SaveClue imgSrc={SaveBtn} text="GUARDAR"/>
+
+      <SaveClue imgSrc={SaveBtn} text="GUARDAR" />
     </ImageBackground>
-    
   );
 };
 
-const windowHeight = Dimensions.get('window').height;
-const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get("window").height;
+const windowWidth = Dimensions.get("window").width;
 
 // Estilos para el componente
 const styles = StyleSheet.create({
   container: {
     flex: 1, // Usa todo el espacio disponible
-    justifyContent: 'center', // Centra el contenido verticalmente
-    alignItems: 'center', // Centra el contenido horizontalmente
-    backgroundColor: '#f0f0f0' // Un fondo gris claro
+    justifyContent: "center", // Centra el contenido verticalmente
+    alignItems: "center", // Centra el contenido horizontalmente
+    backgroundColor: "#f0f0f0", // Un fondo gris claro
   },
   text: {
     fontSize: 20, // Tamaño de fuente grande
-    fontWeight: 'bold' // Texto en negrita
+    fontWeight: "bold", // Texto en negrita
   },
   backgroundImage: {
     flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
+    resizeMode: "cover",
+    justifyContent: "center",
     //alignItems: 'center',
     //padding: 20,
-    },
-    overlay: {
-    position: 'absolute',
+  },
+  overlay: {
+    position: "absolute",
     width: windowWidth,
     height: windowHeight,
-    backgroundColor: 'black',
+    backgroundColor: "black",
     opacity: 0.7,
     left: 0,
     top: 0,
